571fbef4e76294490c1d4016b4edfd39a44c256e
slopguz055 <slopguz055@g.educaand.es>
2025-02-26 23:40:08 +0100

    Add files via upload

---
+++ trigger.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$pusher = new Pusher\Pusher(
+    "xxxxxxxxxxxxxxxxxxxx", // key
+    "xxxxxxxxxxxxxxxxxxxx", // secret
+    "xxxxxxx", // app_id
+    array(
+        'cluster' => 'eu'
+    )
+);
+
+for($i = 0; $i < 15; $i++) {
+    $pusher->trigger('chart-original', 'new-chart', array(
+        rand(0, 5000),rand(0, 5000),rand(0, 5000),rand(0, 5000),rand(0, 5000)
+    ));
+    time_nanosleep(1,0); // Equivale a sleep
+}
